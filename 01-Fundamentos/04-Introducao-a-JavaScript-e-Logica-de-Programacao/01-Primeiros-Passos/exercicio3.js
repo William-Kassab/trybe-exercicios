61eb20ba2a6c7a631f26c728b557714f5ca96572
William <william.mk1@outlook.com>
2021-07-28 17:00:43 -0300

    exercicio 3

---
+++ 01-Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/01-Primeiros-Passos/exercicio3.js
@@ -0,0 +1,18 @@
+let a = 4;
+let b = 6;
+let c = 5;
+
+if (a > b && a > c) {
+  console.log(a);
+}
+
+else if (b > a && b > c) {
+  console.log(b);
+}
+
+else {
+  console.log(c);
+}
+
+
+//Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
