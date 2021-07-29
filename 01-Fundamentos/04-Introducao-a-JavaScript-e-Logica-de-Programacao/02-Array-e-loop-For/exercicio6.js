f1f363e50f446bfbd03daa7dab5f5567d8c78be5
William <william.mk1@outlook.com>
2021-07-29 19:36:39 -0300

    .

--- 01-Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/02-Array-e-loop-For/exercicio6.js
+++ 01-Fundamentos/04-Introducao-a-JavaScript-e-Logica-de-Programacao/02-Array-e-loop-For/exercicio6.js
@@ -1,9 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let minNumber = Math.min(...numbers);
+let biggerNum = numbers[0];
 
-console.log(minNumber);
-
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] < biggerNum) {
+    biggerNum = numbers[index];
+  }
+}
+console.log(biggerNum);
 
 
 //Utilizando for , descubra qual o menor valor contido no array e imprima-o
