79808d0bd645fdb155288dfae59e6bc256e54ecb
William <william.mk1@outlook.com>
2021-08-30 16:47:01 -0300

    .

---
+++ 01-Fundamentos/07-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/02-Objects/exercicio1.js
@@ -0,0 +1,54 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (order) => {
+  console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, Endereço: ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`);
+}
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+  order.name = 'Luiz Silva';
+  order.payment.total = '50,00';
+  console.log(`Olá ${order.name}, o total do seu pedido de ${Object.keys(order.order.pizza)} e ${order.order.drinks.coke.type} é R$ ${order.payment.total}`);
+}
+
+orderModifier(order);
+
+/*Exercícios
+1 - Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+
+2 - Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."*/
