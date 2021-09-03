fea8489ed78f4173f03c7b0a2300a92a34199e67
William <william.mk1@outlook.com>
2021-09-03 20:13:36 -0300

    .

--- 01-Fundamentos/08-Higher-Order-Functions-do-JavaScript-ES6/02-forEach-find-some-every-sort/exercicio1.js
+++ 01-Fundamentos/08-Higher-Order-Functions-do-JavaScript-ES6/02-forEach-find-some-every-sort/exercicio1.js
@@ -63,6 +63,8 @@ const books = [
   },
 ];
 
+/*1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.*/
+
 function authorBornIn1947() {
   const author = books.find((element, index) => books[index].author.birthYear === 1947);
   return author.author.name;
