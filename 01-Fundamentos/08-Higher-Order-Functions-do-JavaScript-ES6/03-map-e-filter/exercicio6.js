0a31506917b1893eac41d1048eb853f6deaf50fd
William <william.mk1@outlook.com>
2021-09-08 19:53:57 -0300

    .

--- 01-Fundamentos/08-Higher-Order-Functions-do-JavaScript-ES6/03-map-e-filter/exercicio6.js
+++ 01-Fundamentos/08-Higher-Order-Functions-do-JavaScript-ES6/03-map-e-filter/exercicio6.js
@@ -63,4 +63,18 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
+/*6 - Crie um array com o nome de todos os livros com mais de 60 anos de publicação.*/
+
+const expectedResult = [
+  'O Senhor dos Anéis',
+  'Fundação',
+  'O Chamado de Cthulhu',
+];
+
+function oldBooks() {
+  const filterBooks = books.filter((book) => (2021 - book.releaseYear) > 60);
+  return filterBooks.map((bookName) => bookName.name);
+}
+console.log(oldBooks());
+
+//assert.deepStrictEqual(oldBooks(), expectedResult);
