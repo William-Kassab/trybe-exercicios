bec32b02b2289c3b7b38816bf4f902f48e030871
William <william.mk1@outlook.com>
2021-09-23 18:37:09 -0300

    .

---
+++ 01-Fundamentos/10-Testes-Automatizados-com-Jest/03-Simulando-Comportamentos/exercicios/exercicio2.js
@@ -0,0 +1,5 @@
+const randomNum = () => Math.floor(Math.random() * 101);
+
+module.exports = randomNum;
+
+/*2 - Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez. Faça os testes necessários.*/
