a84a99aa41a5b40919710aa58ae76d3eba8a51a1
William <william.mk1@outlook.com>
2021-10-04 19:01:32 -0300

    .

--- 02-Front-End/01-Introducao-React/01-React/exercicio-1/exercicio1/src/App.js
+++ 02-Front-End/01-Introducao-React/01-React/exercicio-1/exercicio1/src/App.js
@@ -2,20 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+const arr = ['compromisso1', 'compromisso2', 'compromisso3', 'compromisso4']
+
 const Task = (value) => {
-  return (
-    <li>{value}</li>
-  );
+  return value.map((values) => <li>{values}</li>);
 }
 
 class App extends React.Component {
   render() {
     return (
       <ol>
-        {Task('hello')}
+        {Task(arr)}
       </ol>
     );
   }
 }
 
 export default App;
+
+/*1 - Crie uma lista de tarefas simples*/
